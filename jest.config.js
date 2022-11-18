2ae410e0ecd4a8746be0a4da1fbe904841e8bc2a
Vytenis <wyt.dislike@gmail.com>
2022-11-18 10:37:00 +0200

    Initial skeleton

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const nodeModules = require('./scripts/nodeModules.js')
+
+module.exports = {
+  transform: {
+    '.(ts)$': 'ts-jest',
+  },
+  modulePaths: nodeModules.find(__dirname),
+  cacheDirectory: '<rootDir>/cache/jest',
+  testRegex: '\\.test\\.ts$',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  fakeTimers: {
+    enableGlobally: true,
+  },
+  verbose: false,
+}
