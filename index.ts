3d966669d8e02bba0a9d3250d02122bfb032fb6b
Vytenis <wyt.dislike@gmail.com>
2022-11-23 10:19:09 +0200

    Reduced log level for performance

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import {ELogLevel, info, init} from './lib/utils/log'
 import packageInfo from './package.json'
 import {launchInterface} from './lib/interface'
 
-init('Controller', ELogLevel.verbose)
+init('Controller', ELogLevel.info)
 
 info(`${packageInfo.name} ${packageInfo.version}`)
 info(`by ${packageInfo.author.name}`)
